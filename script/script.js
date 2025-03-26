b138a771f1e491cbb9fed1fa8f0c865ad0ad1c5a
rafa8641 <rafa.margutte@gmail.com>
2025-03-25 23:33:28 -0300

    Contador

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
+// Navegador
+
 document.addEventListener("DOMContentLoaded", () => {
     const menuIcon = document.getElementById("menu_icon");
     const navList = document.getElementById("nav_list");
@@ -32,3 +34,41 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+
+// Contador
+
+const elementoDias = document.querySelector("[data-dias]");
+const elementoHoras = document.querySelector("[data-horas]");
+const elementoMinutos = document.querySelector("[data-minutos]");
+const elementoSegundos = document.querySelector("[data-segundos]");
+
+const render = (dias, horas, minutos, segundos) => {
+    elementoDias.innerHTML = String(dias).padStart("2",0);
+    elementoHoras.innerHTML = String(horas).padStart("2",0);
+    elementoMinutos.innerHTML = String(minutos).padStart("2",0);
+    elementoSegundos.innerHTML = String(segundos).padStart("2",0);
+};
+
+const countdown = () => {
+    const now = new Date();
+    const data = new Date(2025, 4, 17, 15, 0, 0, 0);
+
+    const tempoContador = data - now;
+
+    if(tempoContador < 0){
+        render(0, 0, 0, 0);
+        clearInterval(intervalo);
+        return;
+    }
+
+    const dias = Math.floor(tempoContador / (1000 * 60 * 60 * 24));
+    const horas = Math.floor((tempoContador % (1000 * 60 * 60 * 24)) / (1000  * 60* 60));
+    const minutos = Math.floor((tempoContador % (1000 * 60 * 60)) / (1000 * 60));
+    const segundos = Math.floor((tempoContador % (1000 * 60)) / 1000)
+
+    render(dias, horas, minutos, segundos);
+
+}
+
+const intervalo = setInterval(countdown, 1000);
+countdown();
